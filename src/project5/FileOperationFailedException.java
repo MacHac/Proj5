27a0ac6b7dd1b80c8766901c265b5e17e6899b73
apw24164 <apw24164@MIDITE18498.academic.jtcc.vccs.edu>
2018-04-24 09:51:11 -0400

    Added documentation to FileOperationFailedException.

--- src/project5/FileOperationFailedException.java
+++ src/project5/FileOperationFailedException.java
@@ -1,7 +1,8 @@
 package project5;
 
 /**
- *
+ * This is a custom wrapper for all IO exceptions.  Since I only care about
+ * the message, not the stack trace, this simplifies things.
  * @author apw24164
  */
 public class FileOperationFailedException extends Exception {
@@ -13,7 +14,7 @@ public class FileOperationFailedException extends Exception {
         this.message = message;
         this.e = e;
     }
-
+    
     public String getMessage() {
         return message;
     }
